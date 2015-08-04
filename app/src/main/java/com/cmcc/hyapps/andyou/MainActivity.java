b37636f97bff818bad3a839b42e7b8cc08b05c6b
gaoruishan <grs0515@163.com>
2015-08-04 23:38:14 +0800

    修改包

---
+++ app/src/main/java/com/cmcc/hyapps/andyou/MainActivity.java
@@ -0,0 +1,20 @@
+package com.cmcc.hyapps.andyou;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by gaoruishan on 15/8/4.
+ */
+public class MainActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        initView();
+    }
+
+    private void initView() {
+
+    }
+}
